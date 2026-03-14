3d5b3e851becf8b0325222a976f2dd6b7de5269a
TakkunMCSub <takkunmcjp@gmail.com>
2026-03-14 20:02:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function openTool(tool){
 location.href = tool + "/";
 }
+
+function searchTools(){
+
+let input = document.getElementById("search").value.toLowerCase();
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+let name = card.dataset.name;
+
+if(name.includes(input)){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+}
